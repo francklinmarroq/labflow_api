9311c20024eb73aedf301bf1b65cf3ed6c3e63af
Francklin Marroquin <francklin.marroquin@outlook.com>
2026-05-27 15:39:34 -0600

    feat(config): agregar configuración inicial de Spring Security
    - Incluye dependencia spring-boot-starter-security en pom.xml
    - Crea SecurityConfig con filtros de autorización, sesiones stateless y autenticación básica HTTP
    - Configura UserDetailsService en memoria como punto de partida

---
+++ src/main/java/marroquinsoftware/labflowapi/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package marroquinsoftware.labflowapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) {
+        http.authorizeHttpRequests((requests) -> ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl) requests.anyRequest()).authenticated());
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        //http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+        return (SecurityFilterChain) http.build();
+    }
+
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager();
+    }
+}
